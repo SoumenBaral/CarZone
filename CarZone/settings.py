6ad52c7a098b3afe94fb0d174ed8fbaab1c39ef1
SoumenBaral <shumen.sb@gmail.com>
2024-02-03 20:33:09 +0600

    add requirments

--- CarZone/settings.py
+++ CarZone/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-3r()si8$r6we!vjttg@*aa896(5yrej!#l2y@dtjrv4e*7+nu8
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
